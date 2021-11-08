1c0d6d225affe76c4db0c6a201b3970ddc3dcafe
Vinicius Resende <viniatresnde@gmail.com>
2021-11-08 14:27:04 -0300

    Inicia projeto

---
+++ src/main/java/com/mars/exploremars/ExploremarsApplication.java
@@ -0,0 +1,13 @@
+package com.mars.exploremars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploremarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploremarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mars/exploremars/ExploremarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mars.exploremars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploremarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
